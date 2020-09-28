92d4c9fcaf7b2b3bc850c94eb7b4341240ff5800
Atila Lima <atilamanager@gmail.com>
2020-09-28 12:27:10 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
